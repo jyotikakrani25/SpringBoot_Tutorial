8b6b921d46da8569e6b9a4ceffdcba97b7767dd9
Jyoti Kakrani <jyotikakrani25@gmail.com>
2022-03-01 15:12:14 +0530

    Added Serice annotation

--- src/main/java/com/buddy/tutorial/service/UserService.java
+++ src/main/java/com/buddy/tutorial/service/UserService.java
@@ -1,10 +1,12 @@
 package com.buddy.tutorial.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.buddy.tutorial.model.User;
 import com.buddy.tutorial.repository.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired
