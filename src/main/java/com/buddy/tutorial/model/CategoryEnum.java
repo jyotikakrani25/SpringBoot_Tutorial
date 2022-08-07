ea55cff5c48cd74fa778e1a1191feab0558fd9e3
Jyoti Kakrani <jyotikakrani25@gmail.com>
2022-08-07 12:35:56 +0530

    Fixed PR comments

--- src/main/java/com/buddy/tutorial/model/CategoryEnum.java
+++ src/main/java/com/buddy/tutorial/model/CategoryEnum.java
@@ -1,3 +1,6 @@
 package com.buddy.tutorial.model;
 
-public enum CategoryEnum {business, entertainment, general, health, science, sports, technology}
+public enum CategoryEnum {
+    business, entertainment, general, health, science, sports, technology
+
+}
